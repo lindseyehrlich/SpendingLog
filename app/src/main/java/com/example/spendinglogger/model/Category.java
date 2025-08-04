9de1348311dcafcbab9915e2279cefbb2879fbb4
lehrlich3 <lehrlich3@gatech.edu>
2025-08-04 15:39:47 -0500

    base model classes Category and Payment

---
+++ app/src/main/java/com/example/spendinglogger/model/Category.java
@@ -0,0 +1,17 @@
+package com.example.spendinglogger.model;
+
+public class Category {
+    private String title;
+    private String description;
+
+    public Category(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public String getTitle() { return title; }
+    public String getDescription() { return description; }
+
+    public void setTitle(String title) { this.title = title; }
+    public void setDescription(String description) { this.description = description; }
+}
--- app/src/main/java/com/example/spendinglogger/model/Payment.java
+++ app/src/main/java/com/example/spendinglogger/model/Payment.java
@@ -5,17 +5,84 @@ import java.util.Date;
 public class Payment {
     // Private attributes
     private String title;
-    private double amount;
+    private Double amount;
     private Category category;
     private Date date;
     private String notes;
 
     // Public constructor
-    public Payment(String title, double amount, Category category, Date date, String notes) {
+    public Payment(String title, Double amount, Category category, Date date, String notes) {
+        setTitle(title);
+        setAmount(amount);
+        setCategory(category);
+        setDate(date);
+        setNotes(notes);
+    }
+
+    private void checkString(String string, String item) {
+        if (string == null) {
+            String exceptionString = String.format("%s cannot be null", item);
+            throw new NullPointerException(exceptionString);
+        }
+
+        if (string.isBlank()) {
+            String exceptionString = String.format("%s cannot be empty or whitespace", item);
+            throw new IllegalArgumentException(exceptionString);
+        }
+    }
+
+    private void checkAmount(Double amount) {
+        if (amount == null) {
+            throw new NullPointerException("Amount cannot be null.");
+        }
+
+        if (amount <= 0) {
+            throw new IllegalArgumentException("Amount must be greater than 0.");
+        }
+    }
+
+    private void checkCategory(Category category) {
+        if (category == null) {
+            throw new NullPointerException("Category cannot be null.");
+        }
+    }
+
+    private void checkDate(Date date) {
+        if (date == null) {
+            throw new NullPointerException("Date cannot be null.");
+        }
+    }
+
+    // Getters
+    public String getTitle() { return title; }
+    public Double getAmount() { return amount; }
+    public Category getCategory() { return category; }
+    public Date getDate() { return date; }
+    public String getNotes() { return notes; }
+
+    // Setters
+    public void setTitle(String title) {
+        checkString(title, "Title");
         this.title = title;
+    }
+
+    public void setAmount(Double amount) {
+        checkAmount(amount);
         this.amount = amount;
+    }
+
+    public void setCategory(Category category) {
+        checkCategory(category);
         this.category = category;
+    }
+
+    public void setDate(Date date) {
+        checkDate(date);
         this.date = date;
+    }
+
+    public void setNotes(String notes) {
+        checkString(notes, "Notes");
         this.notes = notes;
     }
 }
